e4c9ab969d74f785a4bc95fcaf8d313d282d01ca
kinik3 <kinik1263@gmail.com>
2020-06-18 02:20:10 +0300

    YapayZeka Uygulama Geliştirme

--- memo-project.js
+++ memo-project.js
@@ -1,15 +1,18 @@
-function lastTenPow(base,pow){
-	var lastTen = 1;
-	while(pow > 0){
-		lastTen = (lastTen * base) % 10000000000;
-		pow--;
-	}
-	return lastTen;
-}
-
-var lastTenSum = 0;
-for(var n = 1; n <= 1000; n++){
-	lastTenSum = (lastTenSum + lastTenPow(n,n)) % 10000000000;
-}
-
-console.log(lastTenSum);
+ 
+var tf = require('@tensorflow/tfjs');
+ 
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 5, activation: 'sigmoid', inputShape: [2]}));
+model.add(tf.layers.dense({ units: 2, activation: 'softmax' , outputShape: [2] }));
+ 
+model.compile({loss: 'categoricalCrossentropy', optimizer: tf.train.adam(0.1)});
+ 
+const x = tf.tensor2d([[0, 0], [0, 1], [1, 0], [1, 1]]);
+const y = tf.tensor2d([[1,0],[0,1],[0,1],[1,0]]);
+ 
+const history = model.fit(x, y, {epochs: 200})
+ .then(()=>{
+  console.log("fit is over")
+  //model.predict(tf.tensor2d([[0, 0], [0, 1], [1, 0], [1, 1]])).print();
+  model.predict(xtrain ).print();
+ });
